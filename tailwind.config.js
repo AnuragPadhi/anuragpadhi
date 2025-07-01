2cf9b7657cccda6ec2f073554b82e4a7635a5cea
Anurag Padhi <anuragpadhidash@gmail.com>
2025-07-01 06:40:17 +0000

    added tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   content: [
-    "./src/**/*.{astro,html,js,jsx,ts,tsx}",
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}",
+    "./public/**/*.html"
   ],
   theme: {
     extend: {},
