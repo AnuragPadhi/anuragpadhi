1f4d0e2c5cc0f9fe74f20419002edc9791db92df
Broveer <70621661+broveer@users.noreply.github.com>
2025-07-05 23:13:21 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://anuragpadhi.github.io',
+  site: 'https://anuragpadhi.github.io/anuragpadhi/',
   base: 'anuragpadhi',
 });
